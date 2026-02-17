82ba0c55ffbce227026f78b040b6fbd1319a4564
Admin <morbinev985@gmail.com>
2026-02-18 01:54:09 +0300

    Объекты и классы

---
+++ Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String firsName;
+    private String lastName;
+
+    public Author(String firsName, String lastName) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+}
